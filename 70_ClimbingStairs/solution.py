386c3ec5d44dd4868e62a66e4ec240cee4c7a78c
ranson <only_focus@163.com>
2018-08-23 22:37:00 +0800

    Add 70 answer

--- 70_ClimbingStairs/solution.py
+++ 70_ClimbingStairs/solution.py
@@ -4,3 +4,21 @@ class Solution:
         :type n: int
         :rtype: int
         """
+        #for not excced the array when input is 1
+        array = [0] * (n+2)
+        array[1] = 1
+        array[2] = 2
+        def getNum(n,array):
+            if n > 2:
+                return array[n-1] + array[n-2]
+            else:
+                return array[n]
+        for i in range(1,n+1):
+            array[i] = getNum(i,array)
+        return array[n]
+
+solution = Solution()
+cnt = 1
+out = solution.climbStairs(cnt)
+
+print("cnt: out:",cnt,out)
